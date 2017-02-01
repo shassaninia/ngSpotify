572f261a8ed40cc256a9b2a2c4793c1ef73841b2
shassaninia <xai.soft80@gmail.com>
2017-02-01 12:06:14 -0500

    alternate route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-app.use(express.static(__dirname + '/app'));
+app.use('/app',express.static(path.join(__dirname,'app')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
@@ -14,4 +14,9 @@ server.listen(process.env.PORT || 3000);
 // When the root director is requested, return the index.html page.
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
+});
+
+
+app.get('/app/main.js',function(req,res){
+    res.sendFile(__dirname + '/app/main.js');
 });
